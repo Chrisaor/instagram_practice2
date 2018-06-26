6d0db30ff6190e2da778774c120647aab9615277
chrisaor <pchpch0070@gmail.com>
2018-06-27 01:11:44 +0900

    post_detail에서 comment목록 출력

--- app/posts/models.py
+++ app/posts/models.py
@@ -5,6 +5,6 @@ class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
 
 class PostComment(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
