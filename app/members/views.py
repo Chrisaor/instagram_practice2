1f27b75b1f63cbeca0c6eae0f07ab8ac26cd7eb6
chrisaor <pchpch0070@gmail.com>
2018-06-27 08:50:59 +0900

    SignupForm을 사용

--- app/members/views.py
+++ app/members/views.py
@@ -2,14 +2,18 @@ from django.contrib.auth import get_user_model
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from members.forms import SignupForm
+
 User = get_user_model()
 
 def signup(request):
     if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
 
-        if username and password:
+
             if User.objects.filter(username=username).exists():
                 return HttpResponse(f'Username {username} is already exist!')
             user = User.objects.create_user(
@@ -17,4 +21,8 @@ def signup(request):
                 password=password,
             )
             return HttpResponse(f'{user.username}, {user.password}')
-    return render(request, 'members/signup.html')
+    form = SignupForm()
+    context = {
+        'form':form,
+    }
+    return render(request, 'members/signup.html', context)
