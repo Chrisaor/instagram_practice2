370fd2979d7f24d1741cbc863fca70195ceb2b58
chrisaor <pchpch0070@gmail.com>
2018-06-27 13:01:24 +0900

    코드 리팩토링 및 raise PermissionDenied처리

--- app/config/urls.py
+++ app/config/urls.py
@@ -18,8 +18,11 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+from config import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
     path('post/', include('posts.urls')),
     path('member/', include('members.urls'))
 ] + static(settings.MEDIA_URL,
--- app/members/forms.py
+++ app/members/forms.py
@@ -65,6 +65,13 @@ class SignupForm(forms.Form):
             }
         )
     )
+    age = forms.IntegerField(
+        widget=forms.NumberInput(
+            attrs={
+                'class':'form-control',
+            }
+        )
+    )
 
     def clean_username(self):
         data = self.cleaned_data['username']
@@ -88,8 +95,12 @@ class SignupForm(forms.Form):
     def _signup(self):
         username = self.cleaned_data['username']
         password = self.cleaned_data['password']
+        age = self.cleaned_data['age']
         return User.objects.create_user(
             username=username,
-            password=password
+            password=password,
+            age=age,
         )
+
+
 
