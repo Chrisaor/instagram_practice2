e55b9b38e72d0294e52b90aa87f2766448f6bf6d
chrisaor <pchpch0070@gmail.com>
2018-06-27 11:57:57 +0900

    Post에 author필드 추가, PostManage에서 get_queryset재정의

--- app/posts/models.py
+++ app/posts/models.py
@@ -1,8 +1,24 @@
+from django.conf import settings
 from django.db import models
 
+class PostManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().exclude(author=None)
+
 class Post(models.Model):
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+    )
     photo = models.ImageField(upload_to='posts')
     created_at = models.DateTimeField(auto_now_add=True)
+
+    objects = PostManager()
+
+    class Meta:
+        ordering = ['-created_at']
 
 class PostComment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
@@ -11,3 +27,6 @@ class PostComment(models.Model):
 
     class Meta:
         ordering = ['-created_at']
+
+
+
